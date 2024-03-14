77391eb5a527af23fe158d4b4ce042a3960c4e7e
GuilhermeSalkovski <guilherme.l.salkovski@exxonmobil.com>
2024-03-14 13:23:05 -0300

    feat: add modal

--- src/pages/components/Header/index.tsx
+++ src/pages/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { HeaderContainer, HeaderContent, HeaderLogo, NewTransactionButton } from "./styles";
 import logoImg from "../../../assets/logo.svg";
+import * as Dialog from '@radix-ui/react-dialog';
 
 export function Header() {
   return (
@@ -9,8 +10,20 @@ export function Header() {
                 <img src={logoImg} alt="" />
                 <h2>DT Money</h2>
             </HeaderLogo>
+              <Dialog.Root>
+                <Dialog.Trigger asChild>
+                  <NewTransactionButton>Nova Transação</NewTransactionButton>
+                </Dialog.Trigger>
 
-            <NewTransactionButton>Nova Transação</NewTransactionButton>
+                <Dialog.Portal>
+                  <Dialog.Overlay className="overlay" />
+                  <Dialog.Content className="content">
+                    <Dialog.Title className="title">Nova Transação</Dialog.Title>
+                    <Dialog.Description className="description">Preencha os campos abaixo para cadastrar uma nova transação</Dialog.Description>
+                    <Dialog.Close className="close">Fechar</Dialog.Close>
+                  </Dialog.Content>
+                </Dialog.Portal>
+              </Dialog.Root>
         </HeaderContent>
     </HeaderContainer>
   )
