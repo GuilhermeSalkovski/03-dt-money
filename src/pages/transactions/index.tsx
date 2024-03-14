6cb1d3f1b73e349cf3f247a1a22ae282c9c11cb9
GuilhermeSalkovski <guilherme.l.salkovski@exxonmobil.com>
2024-03-14 10:08:39 -0300

    feat: add searchbar component

--- src/pages/transactions/index.tsx
+++ src/pages/transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../components/Header";
 import { Summary } from "../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transaction() {
@@ -9,30 +10,31 @@ export function Transaction() {
             <Summary />
 
             <TransactionsContainer>
-            <TransactionsTable>
-                <tbody>
-                    <tr>
-                        <td width="50%">Desenvolvimento de site</td>
-                        <td>
-                            <PriceHighlight variant="income">
-                                R$ 12.000,00
-                            </PriceHighlight>
-                        </td>
-                        <td>Venda</td>
-                        <td>10/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Hamburguer</td>
-                        <td>
-                            <PriceHighlight variant="outcome">
-                                - R$ 59,00
-                            </PriceHighlight>
-                        </td>
-                        <td>Alimentação</td>
-                        <td>10/02/2021</td>
-                    </tr>
-                </tbody>
-            </TransactionsTable>
+                <SearchForm />
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>10/02/2021</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    - R$ 59,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>10/02/2021</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
             </TransactionsContainer>
 
         </div>
