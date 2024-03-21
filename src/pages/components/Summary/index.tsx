0b87903f24095bf123d9110d8dca43a1defd48ba
GuilhermeSalkovski <guilherme.l.salkovski@exxonmobil.com>
2024-03-21 14:29:55 -0300

    feat: move transaction summary calculation to hook

--- src/pages/components/Summary/index.tsx
+++ src/pages/components/Summary/index.tsx
@@ -1,30 +1,10 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
-import { TransactionsContext } from "../../transactions/contexts/TransactionContext";
-import { useContext } from "react";
 import { priceFormatter } from "../../../utils/formatter";
+import { useSummmary } from "../../../hooks/useSummary";
 
 export function Summary() {
-    const { transactions } = useContext(TransactionsContext);
-
-    const summary = transactions.reduce(
-        (acc, transaction) => {
-            if (transaction.type === 'income'){
-                acc.income += transaction.price;
-                acc.total += transaction.price;
-            } else {
-                acc.outcome += transaction.price;
-                acc.total -= transaction.price;
-            }
-            return acc
-        },
-        {
-            income: 0,
-            outcome: 0,
-            total: 0
-        }
-    );
-
+    const summary = useSummmary()
     return(
         <SummaryContainer>
             <SummaryCard>
