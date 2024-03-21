e565c3f5b6c18010aa75d45ae3f2dee06bfdbceb
GuilhermeSalkovski <guilherme.l.salkovski@exxonmobil.com>
2024-03-21 15:48:20 -0300

    feat: add react hook form + zod to search and new transaction form

--- src/pages/components/NewTransactionModal/index.tsx
+++ src/pages/components/NewTransactionModal/index.tsx
@@ -1,35 +1,82 @@
-import * as Dialog from "@radix-ui/react-dialog";
-import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from "./styles";
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as Dialog from '@radix-ui/react-dialog';
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
+
+const newTransactionFormSchema = z.object({
+  description: z.string(),
+  price: z.number(),
+  category: z.string(),
+  // type: z.enum(['income', 'outcome']),
+});
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
-    return (
-        <Dialog.Portal>
-            <Overlay />
-            <Content>
-                <Dialog.Title className="title">Nova Transação</Dialog.Title>
-                <CloseButton><X size={24}/></CloseButton>
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting }
+  } = useForm<NewTransactionFormInputs>({
+    resolver: zodResolver(newTransactionFormSchema),
+  })
 
-                <Dialog.Description className="description"></Dialog.Description>
-                <form action="">
-                    <input type="text" placeholder="Descrição" required />
-                    <input type="number" placeholder="Preço" required />
-                    <input type="text" placeholder="Categoria" required />
+  async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+    await new Promise(resolve => setTimeout(resolve, 2000));
 
-                    <TransactionType>
-                        <TransactionTypeButton variant="income" value="income">
-                            <ArrowCircleUp size={24} />
-                            Entrada
-                        </TransactionTypeButton>
-                        <TransactionTypeButton variant="outcome" value="outcome">
-                            <ArrowCircleDown size={24} />
-                            Saída
-                        </TransactionTypeButton>
+    console.log(data);
+  }
 
-                    </TransactionType>
-                    <button type="submit">Cadastrar</button>
-                </form>
-            </Content>
-      </Dialog.Portal>
-    )
+  return (
+    <Dialog.Portal>
+      <Overlay />
+
+      <Content>
+        <Dialog.Title>Nova Transação</Dialog.Title>
+
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
+        <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+          <input
+            type="text"
+            placeholder="Descrição"
+            required
+            {...register('description')}
+          />
+          <input
+            type="number"
+            placeholder="Preço"
+            required
+            {...register('price', { valueAsNumber: true })}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register('category')}
+          />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income" value="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant="outcome" value="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+
+          <button type="submit" disabled={isSubmitting}>
+            Cadastrar
+          </button>
+        </form>
+      </Content>
+    </Dialog.Portal>
+  );
 }
--- src/pages/components/NewTransactionModal/styles.ts
+++ src/pages/components/NewTransactionModal/styles.ts
@@ -52,7 +52,12 @@ export const Content = styled(Dialog.Content)`
             margin-top: 1.5rem;
             cursor: pointer;
 
-            &:hover {
+            &:disabled {
+                opacity: 0.6;
+                cursor: not-allowed;
+            }
+
+            &:not(:disabled):hover {
                 background: ${props => props.theme["green-700"]};
                 transition: background-color 0.2s;
             }
@@ -98,6 +103,7 @@ export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButt
                             props.theme["red-300"]
         };
     }
+
     &[data-state='unchecked']:hover{
         transition: background-color 0.2s;
         background: ${props => props.theme["gray-600"]};
